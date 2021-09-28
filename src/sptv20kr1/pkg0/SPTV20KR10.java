28a94c866c83a89eb165331f08911d4a2c876646
larissa <user lora-tih@inbox.ru>
2021-09-28 12:37:58 +0300

    test 1

---
+++ src/sptv20kr1/pkg0/SPTV20KR10.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sptv20kr1.pkg0;
+
+/**
+ *
+ * @author user
+ */
+public class SPTV20KR10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Java Hello!");
+    }
+    
+}
